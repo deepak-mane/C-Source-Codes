f347320966b574f82a68222b16fbb3fc4fb5c375
Pabitra Padhy <pabitrapadhycse@gmail.com>
2015-03-21 14:15:27 +0530

    any valid label for switch case

---
+++ anyLabelSwitch.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a= 0;
+	
+	switch(a)
+	{
+	case 1:
+		printf("ONE\n");
+		break;
+	case 2:
+		printf("TWO\n");
+		break;
+	defat:
+		printf("NONE\n");
+	}
+
+	return 0;
+}
